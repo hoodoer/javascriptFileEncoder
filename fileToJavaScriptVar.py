4827f6078065c93e2ce54be2c520ffc39000537d
Drew <34483186+hoodoer@users.noreply.github.com>
2019-11-08 12:29:22 +1100

    Initial upload

---
+++ fileToJavaScriptVar.py
@@ -0,0 +1,68 @@
+#!/usr/bin/python
+#
+#
+# Simple tool to take an arbitrary binary (DLL, exe, etc.) 
+# and convert it to a JavaScript var for including in 
+# XSS Session Riding Payloads 
+#
+# Drew Kirkpatrick
+# drew.kirkpatrick@gmail.com
+# @hoodoer
+#
+
+
+
+import argparse
+import subprocess
+import sys
+import os
+
+
+
+def generateCharEncoding(filename):
+	outputEncoding = subprocess.check_output(['xxd', '-i', filename])
+
+	return outputEncoding
+
+
+
+if __name__ == '__main__':
+	parser = argparse.ArgumentParser()
+	parser.add_argument("--filename", help="the file to encode (DLL, exe, php, zip, etc) as a JavaScript var")
+	args = parser.parse_args()
+
+	if len(sys.argv) < 3:
+		parser.print_help()
+		sys.exit()
+
+	print "Converting file into JavaScript friendly hex..."
+	print
+
+	
+	cArray = generateCharEncoding(args.filename)
+
+
+	# Change the C format to JavaScript format
+	# wonky ass code alert!
+	javaScriptArray = cArray.replace("0x", "\\x")
+	javaScriptArray = javaScriptArray.replace(", ", "")
+	javaScriptArray = javaScriptArray.replace("  ", "'")
+	javaScriptArray = javaScriptArray.replace(",", "' +")
+
+	javaScriptList = javaScriptArray.splitlines()
+
+	# First line....
+	javaScriptData = "var fileData = " + javaScriptList[1]
+
+	# Closing line of data
+	javaScriptList[len(javaScriptList)-3] += "';"
+
+	# Loop through the rest...
+	for i in range(2, len(javaScriptList)-2, 1):
+		javaScriptData += "\n" + javaScriptList[i]
+
+
+	print javaScriptData
+
+
+
